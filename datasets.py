6839d3fd2e136b1ebbf74a69104961c41168555b
tanya26romi <tanyatwinkles26@gmail.com>
2019-04-02 11:19:52 +0530

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cam = cv2.VideoCapture(0)
+Id=raw_input('enter your id')
+sampleNum=0
+while(True):
+    ret, img = cam.read();
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        #a rectangle is formed
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        #incrementing sample number
+        sampleNum=sampleNum+1
+        #saving the captured face in the dataset folder
+        #cv2.imwrite("dataSet/User."+str(Id)+'.'+ str(sampleNum) + ".jpg")
+        cv2.imwrite("dataSet/User." + str(Id) + '.' + str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+
+        #wait for 10 milisec
+        cv2.waitKey(10)
+
+    cv2.imshow('frame',img)
+    #wait for 100 miliseconds
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+    # break if the sample number is morethan 20
+    if sampleNum>100:
+        break
+cam.release()
+cv2.destroyAllWindows()
